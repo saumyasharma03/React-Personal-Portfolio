b4918dfee9a86ceb5edd81102e335fd38a36a03a
saumyaa-sharma <saumya.sharma0915@gmail.com>
2024-03-05 22:26:53 +0530

    changes in app

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import { Projects } from './components/Projects';
 import { Contact } from './components/Contact';
 import {Footer} from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 function App() {
   return (
     <div className="App">
